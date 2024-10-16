0150da10d713e1cf7546b1db3e9ccde0a3e324ed
MeteorOfTime <109677504+MeteorOfTime@users.noreply.github.com>
2024-10-16 20:35:35 +0800

    something new

--- utils/packagemanager.py
+++ utils/packagemanager.py
@@ -14,16 +14,19 @@ def install_pkg(environment, package_name):
         content = json.load(j)
         j.close()
     if content["required_platform"][0] == platform.system() and platform.machine() in content["required_platform"][1]:
-        if os.path.exists(PACKAGE_DIR):
-            shutil.rmtree(PACKAGE_DIR)
-        os.mkdir(PACKAGE_DIR)
+        if not os.path.exists(PACKAGE_DIR):
+            os.mkdir(PACKAGE_DIR)
         os.chdir(PACKAGE_DIR)
-        print("Start to download.")
+        print(f"Start to download {package_name}.")
         timestamp_bef = round(time.time() * 1000)
-        response = get(content["pkg_source"], allow_redirects=True, stream=True)
+        try:
+            response = get(content["pkg_source"], allow_redirects=True, stream=True)
+        except:
+            print("Download failed.")
+            return False
         timestamp_aft = round(time.time() * 1000)
         delta_time = timestamp_aft - timestamp_bef
-        print(f"Download successfully. It took {delta_time/1000} seconds.")
+        print(f"Downloaded successfully. It took {delta_time/1000} seconds.")
         with open(content["local_file_name"], "wb") as f:
             f.write(response.content)
             f.close()
@@ -31,9 +34,36 @@ def install_pkg(environment, package_name):
     else:
         return False
 
-def update_source(environment,package_source):
+def update_source(environment, package_source, package_name):
     PACKAGE_CACHE = environment + os.sep + "cache" + os.sep + "packagemanager" + os.sep + package_name + ".json"
+    print(f"Start to delete {PACKAGE_CACHE}")
+    os.remove(PACKAGE_CACHE)
+    print(f"Deleted successfully. Start to update {package_name}")
+    if not get_source(environment,package_source,package_name):
+        print("Update failed.")
+        return False
+    try:
+        if install_pkg(environment, package_name):
+            print("Updated successfully.")
+            return True
+        else:
+            return False
+    except:
+        print("Update failed.")
+        return False
 
-
+def get_source(environment, package_source, package_name):
+    PACKAGE_CACHE = environment + os.sep + "cache" + os.sep + "packagemanager" + os.sep + package_name + ".json"
+    print(f"Start to download the source of {package_name}")
+    try:
+        response = get(package_source, allow_redirects=True, stream=True)
+    except:
+        print("Download failed.")
+        return False
+    with open(PACKAGE_CACHE, "wb") as f:
+        f.write(response.content)
+        f.close()
+    return True
 if __name__ == "__main__":
-    install_pkg(environment=r"C:\Users\MeteorOfTime\Documents\GitHub\mterminal", package_name="test")
+    get_source(environment=r"C:\Users\MeteorOfTime\Documents\GitHub\mterminal", package_source=r"https://raw.githubusercontent.com/MeteorOfTime/mterminal/main/cache/packagemanager/test1.json", package_name="test1")
+    update_source(environment=r"C:\Users\MeteorOfTime\Documents\GitHub\mterminal", package_source=r"https://raw.githubusercontent.com/MeteorOfTime/mterminal/main/cache/packagemanager/test1.json", package_name="test1")
